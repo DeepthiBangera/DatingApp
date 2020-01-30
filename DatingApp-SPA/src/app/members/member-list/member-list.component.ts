8d92d318b8963fd47515d38ad79c530d68452743
Deepthi Janardhana Bangera <deepthi.b@sonata-software.com>
2020-01-30 16:20:16 +0530

    End of Section 14

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,8 +1,10 @@
+import { PaginatedResult } from './../../_models/pagination';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { User } from 'src/app/_models/User';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination } from 'src/app/_models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -12,23 +14,47 @@ import { ActivatedRoute } from '@angular/router';
 export class MemberListComponent implements OnInit {
 
   users: User[];
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList = [{value: 'male', display: 'males'}, {value: 'female', display: 'females'}];
+  userParams: any = {};
+  pagination: Pagination;
 
   constructor(private userservice: UserService, private alertify: AlertifyService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data['users'];
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
+
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
   }
 
-  // loadUsers() {
-  //   this.userservice.getUsers().subscribe((users: User[]) => {
-  //     this.users = users;
-  //   },
-  //   error => {
-  //     this.alertify.error(error);
-  //   });
-  // }
+  resetFilter() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userservice.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
+    .subscribe((res: PaginatedResult<User[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    },
+    error => {
+      this.alertify.error(error);
+    });
+  }
 
 }
